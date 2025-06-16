e885cca0706977555dcb4a0aea4fad3a84acb8c8
pennA <lucaspsantos003@gmail.com>
2025-06-16 19:43:52 -0300

    Revert "Fix Cors"
    This reverts commit 1a9372095fdb5f734c9ebf5eb691f5122d80eb81.

--- src/services/dashboardService.ts
+++ src/services/dashboardService.ts
@@ -1,13 +1,7 @@
 import prisma from "../prismaClient";
 
 export const getAllDonors = async () => {
-<<<<<<< Updated upstream
   const allDonors = await prisma.doador.findMany({});
-=======
-  const allDonors = await prisma.usuario.findMany({
-    where: { cargo_id: 7 },
-  });
->>>>>>> Stashed changes
 
   return allDonors;
 };
